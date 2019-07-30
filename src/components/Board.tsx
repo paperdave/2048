91b83c523f0eb86bb86973e6d1b4748bf72ba389
Dave Caruso <dave@davecode.me>
2019-07-30 13:01:30 -0400

    Background Thing

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -10,12 +10,8 @@ function Board({ board }: { board: IBoard }) {
     <div className={styles.root}>
       {/* The Board */}
       <svg width="512" height="512" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect width="512" height="512" fill="#252525" />
-        <rect width="10" height="10" fill="#3D3D3D" />
-        <rect y="502" width="10" height="10" fill="#3D3D3D" />
-        <rect x="502" y="502" width="10" height="10" fill="#3D3D3D" />
+        <path d="M10 0H502V10H512V502H502V512H10V502H0V10H10V0Z" fill="#252525" />
         <rect x="30" y="30" width="452" height="452" fill="#555555" />
-        <rect x="502" width="10" height="10" fill="#3D3D3D" />
         <rect x="472" y="472" width="10" height="10" fill="#252525" />
         <rect x="472" y="30" width="10" height="10" fill="#252525" />
         <rect x="30" y="472" width="10" height="10" fill="#252525" />

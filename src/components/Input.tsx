ae026075126bd4c6377bfc0e5df7b18d21f36c97
Dave Caruso <dave@davecode.me>
2019-08-01 20:10:50 -0400

    Touch Controls

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,10 @@
-import { useEffect } from 'react'
+import React, { useEffect, useMemo, useCallback } from 'react'
 import { connect } from 'react-redux';
 import { moveUp, moveDown, moveRight, moveLeft, start } from '../actions';
 import { throttle } from '@reverse/debounce';
+import { Swipeable, defineSwipe } from 'react-touch';
+
+const swipeConfig = defineSwipe({ swipeDistance: 50 });
 
 function Input({
   moveUp,
@@ -9,53 +12,74 @@ function Input({
   moveLeft,
   moveRight,
   reset,
+  disabled,
+  children,
 }: {
   moveUp: Function,
   moveDown: Function,
   moveLeft: Function,
   moveRight: Function,
   reset: Function,
+  disabled: boolean,
+  children: JSX.Element,
 }) {
-  useEffect(() => {
-    const activateKey = throttle(function (key: 'Up' | 'Left' | 'Down' | 'Right' | 'Reset') {
-      switch (key) {
-        case 'Reset': { reset(); break; }
-        case 'Up': { moveUp(); break; }
-        case 'Down': { moveDown(); break; }
-        case 'Left': { moveLeft(); break; }
-        case 'Right': { moveRight(); break; }
-        default:
-          break;
-      }
-    }, 125);
-    function onKeyPress(ev: KeyboardEvent) {
-      switch (ev.key.toLowerCase()) {
-        case 'escape':
-        case 'r':
-          activateKey('Reset'); break;
-        case 'w':
-        case 'arrowup':
-          activateKey('Up'); break;
-        case 's':
-        case 'arrowdown':
-          activateKey('Down'); break;
-        case 'a':
-        case 'arrowleft':
-          activateKey('Left'); break;
-        case 'd':
-        case 'arrowright':
-          activateKey('Right'); break;
-        default:
-      }
+  const activateKey = useMemo(() => throttle(function (key: 'Up' | 'Left' | 'Down' | 'Right' | 'Reset') {
+    switch (key) {
+      case 'Reset': { reset(); break; }
+      case 'Up': { moveUp(); break; }
+      case 'Down': { moveDown(); break; }
+      case 'Left': { moveLeft(); break; }
+      case 'Right': { moveRight(); break; }
+      default:
+        break;
     }
+  }, 125), [moveUp, moveDown, moveLeft, moveRight, reset]);
 
-    window.addEventListener('keydown', onKeyPress);
-    return () => {
-      window.removeEventListener('keydown', onKeyPress);
-    };
-  }, [moveUp, moveDown, moveLeft, moveRight, reset]);
+  const onKeyPress = useCallback((ev: KeyboardEvent) => {
+    switch (ev.key.toLowerCase()) {
+      case 'escape':
+      case 'r':
+        activateKey('Reset'); break;
+      case 'w':
+      case 'arrowup':
+        activateKey('Up'); break;
+      case 's':
+      case 'arrowdown':
+        activateKey('Down'); break;
+      case 'a':
+      case 'arrowleft':
+        activateKey('Left'); break;
+      case 'd':
+      case 'arrowright':
+        activateKey('Right'); break;
+      default:
+    }
+  }, [activateKey]);
 
-  return null;
+  useEffect(() => {
+    if (!disabled) {
+      window.addEventListener('keydown', onKeyPress);
+      return () => {
+        window.removeEventListener('keydown', onKeyPress);
+      };
+    }
+  }, [disabled, onKeyPress]);
+
+  // const activateKeyReset = useCallback(() => activateKey('Reset'), [activateKey])
+  const activateKeyUp = useCallback(() => (console.log('Up'), activateKey('Up')), [activateKey])
+  const activateKeyDown = useCallback(() => (console.log('Down'), activateKey('Down')), [activateKey])
+  const activateKeyLeft = useCallback(() => (console.log('Left'), activateKey('Left')), [activateKey])
+  const activateKeyRight = useCallback(() => (console.log('Right'), activateKey('Right')), [activateKey])
+
+  return <Swipeable
+    onSwipeUp={activateKeyUp}
+    onSwipeDown={activateKeyDown}
+    onSwipeLeft={activateKeyLeft}
+    onSwipeRight={activateKeyRight}
+    config={swipeConfig}
+  >
+    {children}
+  </Swipeable>;
 }
 
 export default connect(
